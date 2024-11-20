5a2a17165eb2325440636121825457456b933554
sajalpaudyal <paudyalsajal@gmail.com>
2024-11-20 21:11:47 +0100

    connectBE:fetch and map air quality data

--- src/app/services/air-quality.service.ts
+++ src/app/services/air-quality.service.ts
@@ -1,32 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { AirQualityData } from '../model/air-quality.data';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, of, map } from "rxjs";
+import { AirQualityData, EnvironmentalData } from "../model/air-quality.data";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AirQualityService {
-  constructor(private http: HttpClientModule) {}
-  private apiUrl = 'http://localhost:8133/api/environmental-data/daily';
+  constructor(private http: HttpClient) {}
+  private apiUrl = "http://localhost:8133/api/environmental-data/daily";
   getAirQualityByHour(): Observable<AirQualityData[]> {
-    // let response = this.http
-    //   .get<EnvironmentalData[]>(this.apiUrl)
-    //   .pipe(tap((response) => console.log(response)));
-    // console.log(response);
-    return of([
-      { hour: 0, value: 25 },
-      { hour: 2, value: 35 },
-      { hour: 4, value: 45 },
-      { hour: 6, value: 30 },
-      { hour: 8, value: 25 },
-      { hour: 10, value: 35 },
-      { hour: 12, value: 45 },
-      { hour: 14, value: 30 },
-      { hour: 16, value: 45 },
-      { hour: 18, value: 30 },
-      { hour: 20, value: 25 },
-      { hour: 22, value: 35 },
-    ]);
+    return this.http.get<EnvironmentalData[]>(this.apiUrl).pipe(
+      map((data: EnvironmentalData[]) =>
+        data.map((item) => ({
+          hour: item.hour,
+          value: item.eco2,
+        })),
+      ),
+    );
   }
 }
